058386f3313e8840fd777af6b1092895ec2dc0fc
HumanHickory <34848650+HumanHickory@users.noreply.github.com>
2019-07-16 15:21:19 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,15 +1,18 @@
+// <!-- Teal, gray and white -->
 
 $(document).ready(function(){
-	$('.primary').css({"color":"#EE693F;"});
-	$('.contrast').css({"color":"#423a35;"});
-	$('.complementary').css({"color":"#739F3D;"});
-	$('.mellow').css({"color":"#f9f8f7"});
+		
+	$('.primary').css({"color":"#1995AD"});
+	$('.contrast').css({"color":"#8c8b8c;"});
+	$('.complementary').css({"color":"#A1D6E2;"});
+	$('.mellow').css({"color":"#F1F1F2"});
 
-	$('.bgPrimary').css({"background-color":"#EE693F;"});
-	$('.bgcontrast').css({"background-color":"#423a35;"});
-	$('.bgComplementary').css({"background-color":"#739F3D;"});
-	$('.bgMellow').css({"background-color":"#f9f8f7"});
+	$('.bgPrimary').css({"background-color":"#1995AD"});
+	$('.bgcontrast').css({"background-color":"#8c8b8c;"});
+	$('.bgComplementary').css({"background-color":"#A1D6E2;"});
+	$('.bgMellow').css({"background-color":"#F1F1F2"});
 
+	document.documentElement.style.setProperty('--primary', '#1995AD');
 });
 
 // <!-- Teal, gray and white -->
@@ -24,71 +27,86 @@ $('#palette1').click(function(){
 	$('.bgcontrast').css({"background-color":"#8c8b8c;"});
 	$('.bgComplementary').css({"background-color":"#A1D6E2;"});
 	$('.bgMellow').css({"background-color":"#F1F1F2"});
+
+	document.documentElement.style.setProperty('--primary', '#1995AD');
+	
 });
 
-// <!-- Pink, Green, Gray, Plum -->
+// <!--Bright Blue, Dark Blue, White, other blue -->
 $('#palette2').click(function(){
-	$('.primary').css({"color":"#EC96A4;"});
-	$('.contrast').css({"color":"#614e63;"});
-	$('.complementary').css({"color":"#9A9EAB;"});
-	$('.mellow').css({"color":"#DFE166"});
+	$('.primary').css({"color":"#3500D3;"});
+	$('.contrast').css({"color":"#0C0032;"});
+	$('.complementary').css({"color":"#C6D166;"});
+	$('.mellow').css({"color":"#FFFFFF"});
 
-	$('.bgPrimary').css({"background-color":"#EC96A4;"});
-	$('.bgcontrast').css({"background-color":"#614e63;"});
-	$('.bgComplementary').css({"background-color":"#9A9EAB;"});
-	$('.bgMellow').css({"background-color":"#DFE166"});
+	$('.bgPrimary').css({"background-color":"#3500D3;"});
+	$('.bgcontrast').css({"background-color":"#0C0032;"});
+	$('.bgComplementary').css({"background-color":"#190061;"});
+	$('.bgMellow').css({"background-color":"#FFFFFF"});
+	
+	document.documentElement.style.setProperty('--primary', '#3500D3');
 });
   
-// <!-- Orange, Navy, Yellow   -->
+// <!-- Bright Red, White, gray, black   -->
 $('#palette3').click(function(){
-	$('.primary').css({"color":"#F0810F;"});
-	$('.contrast').css({"color":"#003b59;"});
-	$('.complementary').css({"color":"#0095e0;"});
-	$('.mellow').css({"color":"#ffe721"});
+	$('.primary').css({"color":"#962715;"});
+	$('.contrast').css({"color":"#1E1E20;"});
+	$('.complementary').css({"color":"#BBC3C6;"});
+	$('.mellow').css({"color":"#FFFFFF"});
 
-	$('.bgPrimary').css({"background-color":"#F0810F;"});
-	$('.bgcontrast').css({"background-color":"#003b59;"});
-	$('.bgComplementary').css({"background-color":"#0095e0;"});
-	$('.bgMellow').css({"background-color":"#ffe721"});
+	$('.bgPrimary').css({"background-color":"#962715;"});
+	$('.bgcontrast').css({"background-color":"#1E1E20;"});
+	$('.bgComplementary').css({"background-color":"#BBC3C6;"});
+	$('.bgMellow').css({"background-color":"#FFFFFF"});
+	
+	document.documentElement.style.setProperty('--primary', '#962715');
 });	
 
-// <!-- Teal, Green, Gray, Forest -->
+// <!-- Pink, Fusha, Red Wine, White -->
 $('#palette4').click(function(){
-	$('.primary').css({"color":"#4CB5F5;"});
-	$('.contrast').css({"color":"#34675C;"});
-	$('.complementary').css({"color":"#b5f024;"});
-	$('.mellow').css({"color":"#f0f2ed;"});
+	$('.primary').css({"color":"#EE4C7C;"});
+	$('.contrast').css({"color":"#5D001E;"});
+	$('.complementary').css({"color":"#9A1750;"});
+	$('.mellow').css({"color":"#FFFFFF;"});
 
-	$('.bgPrimary').css({"background-color":"#4CB5F5;"});
-	$('.bgcontrast').css({"background-color":"#34675C;"});
-	$('.bgComplementary').css({"background-color":"#b5f024;"});
-	$('.bgMellow').css({"background-color":"#f0f2ed;"});
+	$('.bgPrimary').css({"background-color":"#EE4C7C;"});
+	$('.bgcontrast').css({"background-color":"#5D001E;"});
+	$('.bgComplementary').css({"background-color":"#9A1750;"});
+	$('.bgMellow').css({"background-color":"#FFFFFF"});
+	
+	document.documentElement.style.setProperty('--primary', '#EE4C7C');
 });	
 
-//Orange, Green, Brown, White
+//Steel Blue, Yellow, Dark Navy, Beige
 $('#palette5').click(function(){
-	$('.primary').css({"color":"#EE693F;"});
-	$('.contrast').css({"color":"#423a35;"});
-	$('.complementary').css({"color":"#739F3D;"});
-	$('.mellow').css({"color":"#f9f8f7"});
+	$('.primary').css({"color":"#F9BA32;"});
+	$('.contrast').css({"color":"#2F3131;"});
+	$('.complementary').css({"color":"#426E86;"});
+	$('.mellow').css({"color":"#F8F1E5"});
 
-	$('.bgPrimary').css({"background-color":"#EE693F;"});
-	$('.bgcontrast').css({"background-color":"#423a35;"});
-	$('.bgComplementary').css({"background-color":"#739F3D;"});
-	$('.bgMellow').css({"background-color":"#f9f8f7"});
+	$('.bgPrimary').css({"background-color":"#F9BA32;"});
+	$('.bgcontrast').css({"background-color":"#2F3131;"});
+	$('.bgComplementary').css({"background-color":"#426E86;"});
+	$('.bgMellow').css({"background-color":"#F8F1E5"});
+
+	document.documentElement.style.setProperty('--primary', '#F9BA32');
+
 });	
 
-//latte, mocha, mocchiatto, Cinnamon
+//Light Gray, Cobalt, white, black
 $('#palette6').click(function(){
-	$('.primary').css({"color":"#AF4425;"});
-	$('.contrast').css({"color":"#662E1C;"});
-	$('.complementary').css({"color":"#C9A66B;"});
-	$('.mellow').css({"color":"#EBDCB2"});
+	$('.primary').css({"color":"#adadaa;"});
+	$('.contrast').css({"color":"#2F2E33;"});
+	$('.complementary').css({"color":"#3A5199;"});
+	$('.mellow').css({"color":"#FFFFFF"});
 
-	$('.bgPrimary').css({"background-color":"#AF4425;"});
-	$('.bgcontrast').css({"background-color":"#662E1C;"});
-	$('.bgComplementary').css({"background-color":"#C9A66B;"});
-	$('.bgMellow').css({"background-color":"#EBDCB2"});
+	$('.bgPrimary').css({"background-color":"#adadaa;"});
+	$('.bgcontrast').css({"background-color":"#2F2E33;"});
+	$('.bgComplementary').css({"background-color":"#3A5199;"});
+	$('.bgMellow').css({"background-color":"#FFFFFF"});
+	
+	document.documentElement.style.setProperty('--primary', '#adadaa');
+
 });	
 
 
@@ -123,5 +141,8 @@ footerString += "</div>";
 footerString += "</div>";
 
 $('#footer').html(footerString);
+
+
+
 
 
